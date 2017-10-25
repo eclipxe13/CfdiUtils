29a28ed08c3e0c13f8b5ff08325b2b74d2a94f9f
David <driftking301@gmail.com>
2017-10-25 11:14:24 -0500

    Added tests for Conceptos element class

---
+++ tests/CfdiUtilsTests/Elements/Cfdi33/ConceptosTest.php
@@ -0,0 +1,40 @@
+<?php
+namespace CfdiUtilsTests\Elements\Cfdi33;
+
+use CfdiUtils\Elements\Cfdi33\Conceptos;
+use CfdiUtils\Elements\Cfdi33\Concepto;
+use PHPUnit\Framework\TestCase;
+
+class ConceptosTest extends TestCase
+{
+    /** @var Conceptos */
+    public $element;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->element = new Conceptos;
+    }
+
+    public function testGetElementName()
+    {
+        $this->assertSame('cfdi:Conceptos', $this->element->getElementName());
+    }
+
+    public function testAddConcepto()
+    {
+        // no childs
+        $parent = $this->element;
+        $this->assertCount(0, $parent);
+
+        // add first child
+        $first = $parent->addConcepto(['name' => 'first']);
+        $this->assertInstanceOf(Concepto::class, $first);
+        $this->assertSame('first', $first['name']);
+        $this->assertCount(1, $parent);
+
+        // add second child
+        $parent->addConcepto();
+        $this->assertCount(2, $parent);
+    }
+}
