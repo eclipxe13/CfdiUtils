6cb016b243facdebf6c71c424c73d1dbddf04fac
Carlos C Soto <eclipxe13@gmail.com>
2017-10-24 19:22:18 -0500

    Add tests and class that uses InformacionAduaneraTrait

---
+++ tests/CfdiUtilsTests/Elements/Cfdi33/Traits/InformacionAduaneraTraitTest.php
@@ -0,0 +1,38 @@
+<?php
+namespace CfdiUtilsTests\Elements\Cfdi33\Traits;
+
+use CfdiUtils\Elements\Cfdi33\InformacionAduanera;
+use PHPUnit\Framework\TestCase;
+
+class InformacionAduaneraTraitTest extends TestCase
+{
+    public function testAddInformacionAduanera()
+    {
+        // no childs
+        $node = new UseInformacionAduanera('X');
+
+        $this->assertCount(0, $node);
+
+        // add first child
+        $first = $node->addInformacionAduanera(['name' => 'first']);
+        $this->assertInstanceOf(InformacionAduanera::class, $first);
+        $this->assertSame('first', $first['name']);
+        $this->assertCount(1, $node);
+
+        // add second child
+        $node->addInformacionAduanera();
+        $this->assertCount(2, $node);
+    }
+
+    public function testMultiInformacionAduanera()
+    {
+        $node = new UseInformacionAduanera('X');
+        $this->assertCount(0, $node);
+        $node->multiInformacionAduanera(
+            ['id' => 'first'],
+            ['id' => 'second']
+        );
+        $this->assertCount(2, $node);
+        $this->assertSame('first', $node->searchAttribute('cfdi:InformacionAduanera', 'id'));
+    }
+}
---
+++ tests/CfdiUtilsTests/Elements/Cfdi33/Traits/UseInformacionAduanera.php
@@ -0,0 +1,10 @@
+<?php
+namespace CfdiUtilsTests\Elements\Cfdi33\Traits;
+
+use CfdiUtils\Elements\Cfdi33\Traits\InformacionAduaneraTrait;
+use CfdiUtils\Nodes\Node;
+
+class UseInformacionAduanera extends Node
+{
+    use InformacionAduaneraTrait;
+}
