6ff6bfd4006345e6395ae5de599ab0aa3557612d
Carlos C Soto <eclipxe13@gmail.com>
2017-10-25 19:48:15 -0500

    Add validator ComprobanteDecimalesMoneda

---
+++ src/CfdiUtils/Validate/Cfdi33/Standard/ComprobanteDecimalesMoneda.php
@@ -0,0 +1,114 @@
+<?php
+namespace CfdiUtils\Validate\Cfdi33\Standard;
+
+use CfdiUtils\Nodes\NodeInterface;
+use CfdiUtils\Validate\Assert;
+use CfdiUtils\Validate\Asserts;
+use CfdiUtils\Validate\Cfdi33\Abstracts\AbstractDiscoverableVersion33;
+use CfdiUtils\Validate\Status;
+
+/**
+ * ComprobanteDecimalesMoneda
+ *
+ * Valida que:
+ * - MONDEC01: El subtotal del comprobante no contiene más de los decimales de la moneda
+ * - MONDEC02: El descuento del comprobante no contiene más de los decimales de la moneda
+ * - MONDEC03: El total del comprobante no contiene más de los decimales de la moneda
+ * - MONDEC04: El total de impuestos trasladados no contiene más de los decimales de la moneda
+ * - MONDEC05: El total de impuestos retenidos no contiene más de los decimales de la moneda
+ * - MONDEC06: Todos los importes de los traslados no contienen más de los decimales de la moneda
+ * - MONDEC07: Todos los importes de las retenciones no contienen más de los decimales de la moneda
+ */
+class ComprobanteDecimalesMoneda extends AbstractDiscoverableVersion33
+{
+    /** @var Asserts */
+    private $asserts;
+
+    /** @var int */
+    private $precision;
+
+    /** @var string */
+    private $currency;
+
+    private function registerAsserts()
+    {
+        $asserts = [
+            'MONDEC01' => 'El subtotal del comprobante no contiene más de los decimales de la moneda',
+            'MONDEC02' => 'El descuento del comprobante no contiene más de los decimales de la moneda',
+            'MONDEC03' => 'El total del comprobante no contiene más de los decimales de la moneda',
+            'MONDEC04' => 'El total de impuestos trasladados no contiene más de los decimales de la moneda',
+            'MONDEC05' => 'El total de impuestos retenidos no contiene más de los decimales de la moneda',
+            'MONDEC06' => 'Todos los importes de los traslados no contienen más de los decimales de la moneda',
+            'MONDEC07' => 'Todos los importes de las retenciones no contienen más de los decimales de la moneda',
+        ];
+        foreach ($asserts as $code => $title) {
+            $this->asserts->put($code, $title);
+        }
+    }
+
+    public function validate(NodeInterface $comprobante, Asserts $asserts)
+    {
+        $this->asserts = $asserts;
+        $this->registerAsserts();
+
+        $this->currency = $comprobante['Moneda'];
+        $precision = $this->knownCurrencyDecimals();
+        if (null === $precision) {
+            return;
+        }
+        $this->precision = $precision;
+
+        // SubTotal, Descuento, Total
+        $this->validateValue('MONDEC01', $comprobante, 'SubTotal', true);
+        $this->validateValue('MONDEC02', $comprobante, 'Descuento');
+        $this->validateValue('MONDEC03', $comprobante, 'Total', true);
+
+        $impuestos = $comprobante->searchNode('cfdi:Impuestos');
+        if (null !== $impuestos) {
+            $this->validateValue('MONDEC04', $impuestos, 'TotalImpuestosTrasladados');
+            $this->validateValue('MONDEC05', $impuestos, 'TotalImpuestosRetenidos');
+            foreach ($impuestos->searchNodes('cfdi:Traslados', 'cfdi:Traslado') as $impuesto) {
+                if ($this->validateValue('MONDEC06', $impuesto, 'Importe', true)->getStatus()->isError()) {
+                    break;
+                }
+            }
+            foreach ($impuestos->searchNodes('cfdi:Retenciones', 'cfdi:Retencion') as $impuesto) {
+                if ($this->validateValue('MONDEC07', $impuesto, 'Importe', true)->getStatus()->isError()) {
+                    break;
+                }
+            }
+        }
+    }
+
+    private function validateValue(string $code, NodeInterface $node, string $attribute, bool $required = false): Assert
+    {
+        return $this->asserts->putStatus(
+            $code,
+            Status::when($this->checkValue($node, $attribute, $required)),
+            vsprintf('Valor: "%s", Moneda: "%s - %d decimales"', [$node[$attribute], $this->currency, $this->precision])
+        );
+    }
+
+    private function checkValue(NodeInterface $node, string $attribute, bool $required): bool
+    {
+        if ($required && ! isset($node[$attribute])) {
+            return false;
+        }
+        $decimals = pathinfo($node[$attribute] ? : '0', PATHINFO_EXTENSION);
+        return (strlen($decimals) <= $this->precision);
+    }
+
+    /**
+     * @return int|null
+     */
+    private function knownCurrencyDecimals()
+    {
+        $map = [
+            'MXN' => 2,
+            'EUR' => 2,
+            'USD' => 2,
+            'XXX' => 0,
+        ];
+        return array_key_exists($this->currency, $map) ? $map[$this->currency] : null;
+    }
+}
---
+++ tests/CfdiUtilsTests/Validate/Cfdi33/Standard/ComprobanteDecimalesMonedaTest.php
@@ -0,0 +1,107 @@
+<?php
+namespace CfdiUtilsTests\Validate\Cfdi33\Standard;
+
+use CfdiUtils\Nodes\Node;
+use CfdiUtils\Validate\Cfdi33\Standard\ComprobanteDecimalesMoneda;
+use CfdiUtils\Validate\Status;
+use CfdiUtilsTests\Validate\ValidateTestCase;
+
+class ComprobanteDecimalesMonedaTest extends ValidateTestCase
+{
+    /** @var ComprobanteDecimalesMoneda */
+    protected $validator;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->validator = new ComprobanteDecimalesMoneda();
+    }
+
+    public function testUnknownCurrency()
+    {
+        $this->comprobante['Moneda'] = 'LYD'; // Dinar libio
+        $this->runValidate();
+        foreach ($this->asserts as $assert) {
+            $this->assertStatusEqualsStatus(Status::none(), $assert->getStatus());
+        }
+    }
+
+    public function testAllAssertsAreOk()
+    {
+        $this->comprobante->addAttributes([
+            'Moneda' => 'MXN',
+            'SubTotal' => '123',
+            'Descuento' => '1.2',
+            'Total' => '999.99',
+        ]);
+        $this->comprobante->addChild(new Node('cfdi:Impuestos', [
+            'TotalImpuestosTrasladados' => '1.23',
+            'TotalImpuestosRetenidos' => '1.23',
+        ], [
+            new Node('cfdi:Traslados', [], [
+                new Node('cfdi:Traslado', ['Importe' => '123.45']),
+            ]),
+            new Node('cfdi:Retenciones', [], [
+                new Node('cfdi:Retencion', ['Importe' => '123.45']),
+            ]),
+        ]));
+
+        $this->runValidate();
+        foreach ($this->asserts as $assert) {
+            $this->assertStatusEqualsAssert(Status::ok(), $assert);
+        }
+    }
+
+    public function testAllAssertsMissingAttributes()
+    {
+        $this->comprobante->addAttributes([
+            'Moneda' => 'MXN',
+        ]);
+        $this->comprobante->addChild(new Node('cfdi:Impuestos', [
+        ], [
+            new Node('cfdi:Traslados', [], [
+                new Node('cfdi:Traslado'),
+            ]),
+            new Node('cfdi:Retenciones', [], [
+                new Node('cfdi:Retencion'),
+            ]),
+        ]));
+
+        $this->runValidate();
+        $this->assertStatusEqualsCode(Status::error(), 'MONDEC01');
+        $this->assertStatusEqualsCode(Status::ok(), 'MONDEC02');
+        $this->assertStatusEqualsCode(Status::error(), 'MONDEC03');
+        $this->assertStatusEqualsCode(Status::ok(), 'MONDEC04');
+        $this->assertStatusEqualsCode(Status::ok(), 'MONDEC05');
+        $this->assertStatusEqualsCode(Status::error(), 'MONDEC06');
+        $this->assertStatusEqualsCode(Status::error(), 'MONDEC07');
+    }
+
+    public function testAllAssertAreError()
+    {
+        $this->comprobante->addAttributes([
+            'Moneda' => 'MXN',
+            'SubTotal' => '123.000',
+            'Descuento' => '123.000',
+            'Total' => '123.000',
+        ]);
+        $this->comprobante->addChild(new Node('cfdi:Impuestos', [
+            'TotalImpuestosTrasladados' => '123.000',
+            'TotalImpuestosRetenidos' => '123.000',
+        ], [
+            new Node('cfdi:Traslados', [], [
+                new Node('cfdi:Traslado', ['Importe' => '123.00']),
+                new Node('cfdi:Traslado', ['Importe' => '123.000']),
+            ]),
+            new Node('cfdi:Retenciones', [], [
+                new Node('cfdi:Retencion', ['Importe' => '123.00']),
+                new Node('cfdi:Retencion', ['Importe' => '123.000']),
+            ]),
+        ]));
+
+        $this->runValidate();
+        foreach ($this->asserts as $assert) {
+            $this->assertStatusEqualsAssert(Status::error(), $assert);
+        }
+    }
+}
