48155d608cd18013bd42eac479ff66a326b8d531
David <driftking301@gmail.com>
2017-10-27 17:15:41 -0500

    Tests for ComprobanteTotal class

---
+++ tests/CfdiUtilsTests/Validate/Cfdi33/Standard/ComprobanteTotalTest.php
@@ -0,0 +1,69 @@
+<?php
+namespace CfdiUtilsTests\Validate\Cfdi33\Standard;
+
+use CfdiUtils\Validate\Cfdi33\Standard\ComprobanteTotal;
+use CfdiUtils\Validate\Status;
+use CfdiUtilsTests\Validate\ValidateTestCase;
+
+class ComprobanteTotalTest extends ValidateTestCase
+{
+    /** @var ComprobanteTotal */
+    protected $validator;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->validator = new ComprobanteTotal();
+    }
+
+    public function providerTotalWithInvalidValue()
+    {
+        return [
+            'empty' => [''],
+            'missing' => [null],
+            'not a number' => ['foo'],
+            '1.2e3' => ['1.2e3'],
+            ['0.'],
+            ['.0'],
+            ['0..0'],
+            ['0.0.0'],
+            ['-0.0001'],
+        ];
+    }
+
+    /**
+     * @param $value
+     * @dataProvider providerTotalWithInvalidValue
+     */
+    public function testTotalWithInvalidValue($value)
+    {
+        $this->comprobante->addAttributes([
+            'Total' => $value,
+        ]);
+
+        $this->runValidate();
+        $this->assertStatusEqualsCode(Status::error(), 'TOTAL01');
+    }
+
+    public function providerTotalWithValidValues()
+    {
+        return [
+            '0' => ['0'],
+            '0.0' => ['0.0'],
+            '123.45' => ['123.45'],
+        ];
+    }
+
+    /**
+     * @param $value
+     * @dataProvider providerTotalWithValidValues
+     */
+    public function testTotalWithCorrectValues($value)
+    {
+        $this->comprobante->addAttributes([
+            'Total' => $value,
+        ]);
+        $this->runValidate();
+        $this->assertStatusEqualsCode(Status::ok(), 'TOTAL01');
+    }
+}
