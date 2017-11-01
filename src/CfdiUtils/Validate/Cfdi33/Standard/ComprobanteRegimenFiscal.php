270a7a3ffc96b90763b2afb2a7507c0b7fd0106c
David Arzate <driftking301@gmail.com>
2017-11-01 10:13:14 -0600

    Added ComprobanteRegimenFiscal Validator with tests (#13)
    * Added ComprobanteRegimenFiscal Validator with tests
    
    * Removed registerAsserts function, used put function directly instead

---
+++ src/CfdiUtils/Validate/Cfdi33/Standard/ComprobanteRegimenFiscal.php
@@ -0,0 +1,40 @@
+<?php
+namespace CfdiUtils\Validate\Cfdi33\Standard;
+
+use CfdiUtils\Nodes\NodeInterface;
+use CfdiUtils\Validate\Asserts;
+use CfdiUtils\Validate\Cfdi33\Abstracts\AbstractDiscoverableVersion33;
+use CfdiUtils\Validate\Status;
+
+/**
+ * ComprobanteRegimenFiscal
+ *
+ * Valida que:
+ *  - REGFIS01: El régimen fiscal contenga un valor apropiado según el tipo de RFC emisor (CFDI33130 y CFDI33131)
+ */
+class ComprobanteRegimenFiscal extends AbstractDiscoverableVersion33
+{
+    public function validate(NodeInterface $comprobante, Asserts $asserts)
+    {
+        $regimenFiscal = $comprobante->searchAttribute('cfdi:Emisor', 'RegimenFiscal');
+        $emisorRfc = $comprobante->searchAttribute('cfdi:Emisor', 'Rfc');
+
+        $validMoralCodes = [
+            '601', '603', '609', '620', '623', '624', '628', '607', '610', '622',
+        ];
+        $validFisicaCodes = [
+            '605', '606', '608', '611', '612', '614', '616', '621', '629', '630', '615', '610', '622',
+        ];
+
+        $length = strlen($emisorRfc);
+        $validation = ($length === 12 && in_array($regimenFiscal, $validMoralCodes, true))
+            || ($length === 13 && in_array($regimenFiscal, $validFisicaCodes, true));
+
+        $asserts->put(
+            'REGFIS01',
+            'El régimen fiscal contenga un valor apropiado según el tipo de RFC emisor (CFDI33130 y CFDI33131)',
+            Status::when($validation),
+            sprintf('Rfc: "%s", Regimen Fiscal: "%s"', $emisorRfc, $regimenFiscal)
+        );
+    }
+}
---
+++ tests/CfdiUtilsTests/Validate/Cfdi33/Standard/ComprobanteRegimenFiscalTest.php
@@ -0,0 +1,93 @@
+<?php
+namespace CfdiUtilsTests\Validate\Cfdi33\Standard;
+
+use CfdiUtils\Nodes\Node;
+use CfdiUtils\Validate\Cfdi33\Standard\ComprobanteRegimenFiscal;
+use CfdiUtils\Validate\Status;
+use CfdiUtilsTests\Validate\ValidateTestCase;
+
+class ComprobanteRegimenFiscalTest extends ValidateTestCase
+{
+    /** @var  ComprobanteRegimenFiscal */
+    protected $validator;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->validator = new ComprobanteRegimenFiscal();
+    }
+
+    public function providerValidCases()
+    {
+        return[
+            // personas morales
+            ['AAA010101AAA', '601'],
+            ['OEKLGO048LSA', '603'],
+            ['OEKLGO048LSA', '609'],
+            ['AAA010101AAA', '610'],
+            ['DJEIG04KGILD', '620'],
+            ['DJEIG04KGILD', '622'],
+            ['KKGO40FKIWG0', '623'],
+            ['KKWEIGJ34978', '624'],
+            ['LLROR04KKG94', '628'],
+            ['SLLWIO9034LK', '607'],
+            // personas físicas
+            ['AAA010101AAAA', '605'],
+            ['OEKLGO048LSAS', '606'],
+            ['DJEIG04KGILDD', '608'],
+            ['OEKLGO048LSAS', '610'],
+            ['KKGO40FKIWG0S', '611'],
+            ['KKWEIGJ34978G', '612'],
+            ['LLROR04KKG94Q', '614'],
+            ['SLLWIO9034LKG', '616'],
+            ['LSIKIEG03KGIE', '621'],
+            ['KKGO40FKIWG0S', '622'],
+            ['LLSORGJ3098G8', '629'],
+            ['IOEUGKI49G49J', '630'],
+            ['JJJSID98KF0SL', '615'],
+        ];
+    }
+
+    /**
+     * @param $emisorRfc
+     * @param $regimenFiscal
+     * @dataProvider providerValidCases
+     */
+    public function testValidCases($emisorRfc, $regimenFiscal)
+    {
+        $this->comprobante->addChild(new Node('cfdi:Emisor', [
+            'RegimenFiscal' => $regimenFiscal,
+            'Rfc' => $emisorRfc,
+        ]));
+
+        $this->runValidate();
+        $this->assertStatusEqualsCode(Status::ok(), 'REGFIS01');
+    }
+    public function providerInvalidCases()
+    {
+        return [
+            ['AAA010101AAA', '605'],
+            ['OEKLGO048LSAS', '601'],
+            ['', '615'],
+            ['', ''],
+            [null, ''],
+            [null, '630'],
+            [null, null],
+        ];
+    }
+    /**
+     * @param $emisorRfc
+     * @param $regimenFiscal
+     * @dataProvider providerInvalidCases
+     */
+    public function testInvalidCases($emisorRfc, $regimenFiscal)
+    {
+        $this->comprobante->addChild(new Node('cfdi:Emisor', [
+            'RegimenFiscal' => $regimenFiscal,
+            'Rfc' => $emisorRfc,
+        ]));
+
+        $this->runValidate();
+        $this->assertStatusEqualsCode(Status::error(), 'REGFIS01');
+    }
+}
