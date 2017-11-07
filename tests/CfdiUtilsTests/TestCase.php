86a27838013150f10c033d28ad66e3a3579c3e94
David <driftking301@gmail.com>
2017-11-07 14:42:26 -0600

    added providerFullJoin() function

--- tests/CfdiUtilsTests/TestCase.php
+++ tests/CfdiUtilsTests/TestCase.php
@@ -15,4 +15,22 @@ class TestCase extends \PHPUnit\Framework\TestCase
         $xmlResolver = new XmlResolver();
         return $xmlResolver->resolve($remote);
     }
+
+    public function providerFullJoin(array $first, array ...$next): array
+    {
+        if (! count($next)) {
+            return $first;
+        }
+        $combine = [];
+        $second = array_shift($next);
+        foreach ($first as $a) {
+            foreach ($second as $b) {
+                $combine[] = array_merge($a, $b);
+            }
+        }
+        if (count($next)) {
+            return $this->providerFullJoin($combine, ...$next);
+        }
+        return $combine;
+    }
 }
