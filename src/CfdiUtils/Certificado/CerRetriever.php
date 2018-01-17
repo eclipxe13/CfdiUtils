5ceadc2cd47fbce585d9951b219c47398a6f8c4b
Carlos C Soto <eclipxe13@gmail.com>
2018-01-17 16:04:43 -0600

    Create class CerRetriever and include it into XmlResolver to download standard certificate files from SAT

---
+++ src/CfdiUtils/Certificado/CerRetriever.php
@@ -0,0 +1,27 @@
+<?php
+namespace CfdiUtils\Certificado;
+
+use XmlResourceRetriever\AbstractBaseRetriever;
+use XmlResourceRetriever\RetrieverInterface;
+
+class CerRetriever extends AbstractBaseRetriever implements RetrieverInterface
+{
+    public function retrieve(string $resource): string
+    {
+        $this->clearHistory();
+        $localFilename = $this->download($resource);
+        $this->addToHistory($resource, $localFilename);
+        return $localFilename;
+    }
+
+    protected function checkIsValidDownloadedFile(string $source, string $localpath)
+    {
+        // check content is cer file
+        try {
+            new Certificado($localpath);
+        } catch (\Throwable $ex) {
+            unlink($localpath);
+            throw new \RuntimeException("The source $source is not a cer file", 0, $ex);
+        }
+    }
+}
---
+++ tests/CfdiUtilsTests/Certificado/CerRetrieverTest.php
@@ -0,0 +1,44 @@
+<?php
+namespace CfdiUtilsTests\Certificado;
+
+use CfdiUtils\Certificado\Certificado;
+use CfdiUtils\Certificado\SatCertificateNumber;
+use CfdiUtilsTests\TestCase;
+
+class CerRetrieverTest extends TestCase
+{
+    public function testRetrieveNonExistent()
+    {
+        // this certificate does not exists in the internet repository, it will fail to download
+        $certificateId = '20001000000300022779';
+        $cerNumber = new SatCertificateNumber($certificateId);
+        $retriever = $this->newResolver()->newCerRetriever();
+        $remoteUrl = $cerNumber->remoteUrl();
+
+        $this->expectException(\RuntimeException::class);
+        $this->expectExceptionMessage($remoteUrl);
+        $retriever->retrieve($remoteUrl);
+    }
+
+    public function testRetrieveValidCertificate()
+    {
+        // NOTE: This certificate is valid until 2021-05-22 12:42:41
+        // after this date this test may fail
+        $certificateId = '00001000000406258094';
+        $cerNumber = new SatCertificateNumber($certificateId);
+        $retriever = $this->newResolver()->newCerRetriever();
+        $remoteUrl = $cerNumber->remoteUrl();
+        $localPath = $retriever->buildPath($remoteUrl);
+
+        if (file_exists($localPath)) {
+            unlink($localPath);
+        }
+        $this->assertFileNotExists($localPath);
+
+        $retriever->retrieve($remoteUrl);
+        $this->assertFileExists($localPath);
+
+        $certificate = new Certificado($localPath);
+        $this->assertSame($certificateId, $certificate->getSerial());
+    }
+}
