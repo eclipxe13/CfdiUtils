d49f5bcc6ec7265b42728cb91beb1bce0a6de2a2
Carlos C Soto <eclipxe13@gmail.com>
2018-01-17 13:12:33 -0600

    Add support for ImpuestosLocales and fix sum of total

--- tests/CfdiUtilsTests/SumasConceptos/SumasConceptosTest.php
+++ tests/CfdiUtilsTests/SumasConceptos/SumasConceptosTest.php
@@ -2,6 +2,7 @@
 namespace CfdiUtilsTests\SumasConceptos;
 
 use CfdiUtils\Elements\Cfdi33\Comprobante;
+use CfdiUtils\Elements\ImpLocal10\ImpuestosLocales;
 use CfdiUtils\Nodes\Node;
 use CfdiUtils\SumasConceptos\SumasConceptos;
 use PHPUnit\Framework\TestCase;
@@ -18,8 +19,12 @@ class SumasConceptosTest extends TestCase
         $this->assertEquals(0, $sc->getDescuento(), '', $maxDiff);
         $this->assertEquals(0, $sc->getImpuestosRetenidos(), '', $maxDiff);
         $this->assertEquals(0, $sc->getImpuestosTrasladados(), '', $maxDiff);
+        $this->assertEquals(0, $sc->getLocalesImpuestosRetenidos(), '', $maxDiff);
+        $this->assertEquals(0, $sc->getLocalesImpuestosTrasladados(), '', $maxDiff);
         $this->assertCount(0, $sc->getRetenciones());
         $this->assertCount(0, $sc->getTraslados());
+        $this->assertCount(0, $sc->getLocalesRetenciones());
+        $this->assertCount(0, $sc->getLocalesTraslados());
     }
 
     public function providerWithConceptsDecimals()
@@ -69,5 +74,50 @@ class SumasConceptosTest extends TestCase
         $this->assertEquals(0, $sc->getDescuento(), '', $maxDiff);
         $this->assertEquals(0, $sc->getImpuestosRetenidos(), '', $maxDiff);
         $this->assertCount(0, $sc->getRetenciones());
+    }
+
+    public function testWithImpuestosLocales()
+    {
+        $taxDecimals = 4;
+        $maxDiff = 0.0000001;
+        $comprobante = new Comprobante();
+        $comprobante->addConcepto([
+            'Importe' => '111.11',
+        ])->addTraslado([
+            'Impuesto' => '002',
+            'TipoFactor' => 'Tasa',
+            'TasaOCuota' => '0.160000',
+            'Importe' => number_format(111.11 * 0.16, $taxDecimals, '.', ''),
+        ]);
+        $comprobante->addConcepto([
+            'Importe' => '222.22',
+        ])->addTraslado([
+            'Impuesto' => '002',
+            'TipoFactor' => 'Tasa',
+            'TasaOCuota' => '0.160000',
+            'Importe' => number_format(222.22 * 0.16, $taxDecimals, '.', ''),
+        ]);
+        $impuestosLocales = new ImpuestosLocales();
+        $impuestosLocales->addTrasladoLocal([
+            'ImpLocTrasladado' => 'IH', // fixed, taken from a sample,
+            'TasadeTraslado' => '2.5',
+            'Importe' => number_format(333.33 * 0.025, 2, '.', ''),
+        ]);
+        $comprobante->getComplemento()->add($impuestosLocales);
+        $sc = new SumasConceptos($comprobante, 2);
+
+        $this->assertCount(1, $sc->getTraslados());
+        $this->assertCount(1, $sc->getLocalesTraslados());
+
+        $this->assertEquals(333.33, $sc->getSubTotal(), '', $maxDiff);
+        $this->assertEquals(53.33, $sc->getImpuestosTrasladados(), '', $maxDiff);
+        $this->assertEquals(8.33, $sc->getLocalesImpuestosTrasladados(), '', $maxDiff);
+        $this->assertEquals(333.33 + 53.33 + 8.33, $sc->getTotal(), '', $maxDiff);
+        // this are zero
+        $this->assertEquals(0, $sc->getDescuento(), '', $maxDiff);
+        $this->assertEquals(0, $sc->getImpuestosRetenidos(), '', $maxDiff);
+        $this->assertCount(0, $sc->getRetenciones());
+        $this->assertEquals(0, $sc->getLocalesImpuestosRetenidos(), '', $maxDiff);
+        $this->assertCount(0, $sc->getLocalesRetenciones());
     }
 }
