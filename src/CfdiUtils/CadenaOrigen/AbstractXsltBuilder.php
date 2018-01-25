77cc4f082724bcae5ee957a639fc38093e114b40
Carlos C Soto <eclipxe13@gmail.com>
2018-01-24 21:57:22 -0600

    Add abstract class to avoid duplicate of checking input parameters

---
+++ src/CfdiUtils/CadenaOrigen/AbstractXsltBuilder.php
@@ -0,0 +1,16 @@
+<?php
+namespace CfdiUtils\CadenaOrigen;
+
+abstract class AbstractXsltBuilder implements XsltBuilderInterface
+{
+    protected function assertBuildArguments(string $xmlContent, string $xsltLocation): string
+    {
+        if ('' === $xmlContent) {
+            throw new \UnexpectedValueException('The XML content to transform is empty');
+        }
+        if ('' === $xsltLocation) {
+            throw new \UnexpectedValueException('Xslt location was not set');
+        }
+        return '';
+    }
+}
