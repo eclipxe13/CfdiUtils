ef9768805f9f5399eb963a7ed133d6b47759acca
Carlos C Soto <eclipxe13@gmail.com>
2018-01-24 22:01:43 -0600

    Add GenkgoXslBuilder as an implementation of XsltBuilderInterface
    Also include genkgo/xsl package as suggested and development dependence

---
+++ tests/CfdiUtilsTests/CadenaOrigen/GenkgoXslBuilderTest.php
@@ -0,0 +1,13 @@
+<?php
+namespace CfdiUtilsTests\CadenaOrigen;
+
+use CfdiUtils\CadenaOrigen\GenkgoXslBuilder;
+use CfdiUtils\CadenaOrigen\XsltBuilderInterface;
+
+class GenkgoXslBuilderTest extends GenericBuilderTestCase
+{
+    protected function createBuilder(): XsltBuilderInterface
+    {
+        return new GenkgoXslBuilder();
+    }
+}
