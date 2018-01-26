1f9e50cacaeffdc00f530dff27d8d10cc8296aa8
Carlos C Soto <eclipxe13@gmail.com>
2018-01-25 20:32:44 -0600

    Add element TimbreFiscalDigital

---
+++ src/CfdiUtils/Elements/Tfd11/TimbreFiscalDigital.php
@@ -0,0 +1,22 @@
+<?php
+namespace CfdiUtils\Elements\Tfd11;
+
+use CfdiUtils\Elements\Common\AbstractElement;
+
+class TimbreFiscalDigital extends AbstractElement
+{
+    public function getElementName(): string
+    {
+        return 'tfd:TimbreFiscalDigital';
+    }
+
+    public function getFixedAttributes(): array
+    {
+        return [
+            'xmlns:tfd' => 'http://www.sat.gob.mx/TimbreFiscalDigital',
+            'xsi:schemaLocation' => 'http://www.sat.gob.mx/TimbreFiscalDigital'
+                . ' http://www.sat.gob.mx/sitio_internet/cfd/TimbreFiscalDigital/TimbreFiscalDigitalv11.xsd',
+            'Version' => '1.1',
+        ];
+    }
+}
