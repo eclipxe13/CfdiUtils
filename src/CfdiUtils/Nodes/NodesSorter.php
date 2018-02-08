882eacb1c791c5a23e709de66b6fdcd5c6aa9134
Carlos C Soto <eclipxe13@gmail.com>
2018-02-07 21:19:51 -0600

    Add NodesSorter to sort a NodeInterface array by name and position

---
+++ src/CfdiUtils/Nodes/NodesSorter.php
@@ -0,0 +1,109 @@
+<?php
+namespace CfdiUtils\Nodes;
+
+/**
+ * This class is used to sort elements by name in a Nodes collection
+ * @internal
+ */
+class NodesSorter
+{
+    /** @var string[] array of key (string) value (int) representing the naming order */
+    private $order;
+
+    /** @var int */
+    private $count;
+
+    public function __construct(array $order = [])
+    {
+        $this->setOrder($order);
+    }
+
+    /**
+     * It takes only the unique string names and sort using the order of appearance
+     * @param string[] $names
+     * @return bool true if the new names list is different from previous
+     */
+    public function setOrder(array $names): bool
+    {
+        $order = array_flip($this->parseNames($names));
+        if ($this->order === $order) {
+            return false;
+        }
+        $this->order = $order;
+        $this->count = count($order);
+        return true;
+    }
+
+    /**
+     * @param array $names
+     * @return string[]
+     */
+    public function parseNames(array $names): array
+    {
+        $isValidName = function ($name): bool {
+            return is_string($name) && (bool) $name;
+        };
+        return array_values(array_unique(array_filter($names, $isValidName)));
+    }
+
+    /**
+     * The current order list
+     * @return string[]
+     */
+    public function getOrder(): array
+    {
+        return array_flip($this->order);
+    }
+
+    /**
+     * @param NodeInterface[] $nodes
+     * @return NodeInterface[]
+     */
+    public function sort(array $nodes): array
+    {
+        if ($this->count > 0) {
+            // do not use simple usort since usort is not "stable"
+            // usort does not respect the previous order.
+            $nodes = $this->stableArraySort($nodes, [$this, 'compareNodesByName']);
+        }
+        return $nodes;
+    }
+
+    public function compareNodesByName(NodeInterface $a, NodeInterface $b)
+    {
+        return $this->valueByName($a->name()) <=> $this->valueByName($b->name());
+    }
+
+    public function valueByName(string $name): int
+    {
+        return $this->order[$name] ?? $this->count;
+    }
+
+    /*
+     * This function is a replacement for usort that try to usort
+     * but if items are equal then uses the relative position as second argument
+     */
+    private function stableArraySort(array $input, callable $function): array
+    {
+        // create the item list with the item and the index
+        $list = [];
+        foreach (array_values($input) as $i => $value) {
+            $item = new \stdClass();
+            $item->item = $value;
+            $item->index = $i;
+            $list[] = $item;
+        }
+
+        // perform the usort, if comparison is equal then compare the index also
+        usort($list, function (\stdClass $first, \stdClass $second) use ($function) {
+            $value = $function($first->item, $second->item);
+            if (0 === $value) {
+                $value = $first->index <=> $second->index;
+            }
+            return $value;
+        });
+
+        // return only the items
+        return array_column($list, 'item');
+    }
+}
---
+++ tests/CfdiUtilsTests/Nodes/NodesSorterTest.php
@@ -0,0 +1,76 @@
+<?php
+namespace CfdiUtilsTests\Nodes;
+
+use CfdiUtils\Nodes\Node;
+use CfdiUtils\Nodes\NodesSorter;
+use PHPUnit\Framework\TestCase;
+
+class NodesSorterTest extends TestCase
+{
+    public function testConstructWithNames()
+    {
+        $values = ['foo', 'bar', 'baz'];
+        $sorter = new NodesSorter($values);
+        $this->assertSame($values, $sorter->getOrder());
+    }
+
+    public function testConstructWithoutNames()
+    {
+        $sorter = new NodesSorter();
+        $this->assertSame([], $sorter->getOrder());
+    }
+
+    public function testParseNames()
+    {
+        $sorter = new NodesSorter();
+        // all invalid values
+        $this->assertSame([], $sorter->parseNames([null, new \stdClass(), 0, false, '']));
+        // all valid values
+        $this->assertSame(['foo', 'bar'], $sorter->parseNames(['foo', 'bar']));
+        // suplicated values
+        $this->assertSame(['foo', 'bar', 'baz'], $sorter->parseNames(['foo', 'bar', 'bar', 'foo', 'baz']));
+        // mixed values
+        $this->assertSame(['foo', 'bar'], $sorter->parseNames(['', 'foo', '', 'bar', '', 'foo']));
+    }
+
+    public function testSetGetOrder()
+    {
+        $sorter = new NodesSorter(['foo', 'bar']);
+        $this->assertSame(['foo', 'bar'], $sorter->getOrder());
+
+        // it change
+        $this->assertTrue($sorter->setOrder(['bar', 'foo']));
+        $this->assertSame(['bar', 'foo'], $sorter->getOrder());
+
+        // it did not change
+        $this->assertFalse($sorter->setOrder(['bar', 'foo']));
+        $this->assertSame(['bar', 'foo'], $sorter->getOrder());
+    }
+
+    public function testOrder()
+    {
+        $foo1 = new Node('foo');
+        $foo2 = new Node('foo');
+        $bar = new Node('bar');
+        $baz = new Node('baz');
+        $yyy = new Node('yyy');
+
+        $order = ['baz', 'bar', 'foo'];
+        $unsorted = [$yyy, $foo1, $foo2, $bar, $baz];
+        $expected = [$baz, $bar, $foo1, $foo2, $yyy];
+
+        $sorter = new NodesSorter($order);
+        $sorted = $sorter->sort($unsorted);
+        $this->assertSame($expected, $sorted);
+    }
+
+    public function testOrderPreservePosition()
+    {
+        $list = [];
+        for ($i = 0; $i < 1000; $i++) {
+            $list[] = new Node('foo');
+        }
+        $sorter = new NodesSorter(['foo']);
+        $this->assertSame($list, $sorter->sort($list));
+    }
+}
