51c049258c45d5d98d5ee9008e28acc481424c9b
Carlos C Soto <eclipxe13@gmail.com>
2018-05-24 17:34:13 -0500

    Add pagos/pago validator MontoGreaterThanZero

---
+++ tests/CfdiUtilsTests/Validate/Cfdi33/RecepcionPagos/Pagos/MontoGreaterThanZeroTest.php
@@ -0,0 +1,44 @@
+<?php
+namespace CfdiUtilsTests\Validate\Cfdi33\RecepcionPagos\Pagos;
+
+use CfdiUtils\Elements\Pagos10\Pago;
+use CfdiUtils\Validate\Cfdi33\RecepcionPagos\Pagos\MontoGreaterThanZero;
+use CfdiUtils\Validate\Cfdi33\RecepcionPagos\Pagos\ValidatePagoException;
+use PHPUnit\Framework\TestCase;
+
+class MontoGreaterThanZeroTest extends TestCase
+{
+    /**
+     * @param string $amount
+     * @testWith ["0.000001"]
+     *           ["1"]
+     */
+    public function testValid($amount)
+    {
+        $pago = new Pago([
+            'Monto' => $amount,
+        ]);
+        $validator = new MontoGreaterThanZero();
+        $this->assertTrue($validator->validatePago($pago));
+    }
+
+    /**
+     * @param string|null $amount
+     * @testWith ["0.0000001"]
+     *           ["0"]
+     *           ["-1"]
+     *           [null]
+     *           [""]
+     *           ["not numeric"]
+     */
+    public function testPagoMontoGreaterThanZeroInvalid($amount)
+    {
+        $pago = new Pago([
+            'Monto' => $amount,
+        ]);
+        $validator = new MontoGreaterThanZero();
+
+        $this->expectException(ValidatePagoException::class);
+        $validator->validatePago($pago);
+    }
+}
