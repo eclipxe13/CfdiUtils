b315fcd46790e6f56e82a09e93f0027a15eeee43
Carlos C Soto <eclipxe13@gmail.com>
2018-05-24 17:10:29 -0500

    Add pagos/pago/doctorelacionado validator ImporteSaldoAnteriorRequerido

---
+++ src/CfdiUtils/Validate/Cfdi33/RecepcionPagos/Pagos/DoctoRelacionado/ImporteSaldoAnteriorRequerido.php
@@ -0,0 +1,24 @@
+<?php
+namespace CfdiUtils\Validate\Cfdi33\RecepcionPagos\Pagos\DoctoRelacionado;
+
+use CfdiUtils\Nodes\NodeInterface;
+
+/**
+ * PAGO32: En un documento relacionado, el saldo anterior es requerido cuando
+ *         el tipo de cambio existe o existe más de un documento relacionado (CRP234)
+ */
+class ImporteSaldoAnteriorRequerido extends AbstractDoctoRelacionadoValidator
+{
+    protected $code = 'PAGO32';
+    protected $title = 'En un documento relacionado, el saldo anterior es requerido cuando'
+        . ' el tipo de cambio existe o existe más de un documento relacionado (CRP234)';
+
+    public function validateDoctoRelacionado(NodeInterface $docto): bool
+    {
+        if (! isset($docto['ImpSaldoAnt']) && 'PPD' === $docto['MetodoDePagoDR']) {
+            throw $this->exception('No hay saldo anterior y el método de pago es PPD');
+        }
+
+        return true;
+    }
+}
---
+++ tests/CfdiUtilsTests/Validate/Cfdi33/RecepcionPagos/Pagos/DoctoRelacionado/ImporteSaldoAnteriorRequeridoTest.php
@@ -0,0 +1,35 @@
+<?php
+namespace CfdiUtilsTests\Validate\Cfdi33\RecepcionPagos\Pagos\DoctoRelacionado;
+
+use CfdiUtils\Elements\Pagos10\DoctoRelacionado;
+use CfdiUtils\Validate\Cfdi33\RecepcionPagos\Pagos\DoctoRelacionado\ImporteSaldoAnteriorRequerido;
+use CfdiUtils\Validate\Cfdi33\RecepcionPagos\Pagos\DoctoRelacionado\ValidateDoctoException;
+use PHPUnit\Framework\TestCase;
+
+class ImporteSaldoAnteriorRequeridoTest extends TestCase
+{
+    public function testValid()
+    {
+        $docto = new DoctoRelacionado([
+            'MetodoDePagoDR' => 'PPD',
+            'ImpSaldoAnt' => '1',
+        ]);
+        $validator = new ImporteSaldoAnteriorRequerido();
+        $validator->setIndex(0);
+
+        $this->assertTrue($validator->validateDoctoRelacionado($docto));
+    }
+
+    public function testInvalid()
+    {
+        $docto = new DoctoRelacionado([
+            'MetodoDePagoDR' => 'PPD',
+            'ImpSaldoAnt' => null,
+        ]);
+        $validator = new ImporteSaldoAnteriorRequerido();
+        $validator->setIndex(0);
+
+        $this->expectException(ValidateDoctoException::class);
+        $validator->validateDoctoRelacionado($docto);
+    }
+}
