41d3b505a97d4bdf2fe723e8e6f76a2ed6fc011b
Carlos C Soto <eclipxe13@gmail.com>
2018-05-24 17:07:31 -0500

    Add pagos/pago/doctorelacionado validator ImportePagadoRequerido

---
+++ src/CfdiUtils/Validate/Cfdi33/RecepcionPagos/Pagos/DoctoRelacionado/ImportePagadoRequerido.php
@@ -0,0 +1,30 @@
+<?php
+namespace CfdiUtils\Validate\Cfdi33\RecepcionPagos\Pagos\DoctoRelacionado;
+
+use CfdiUtils\Nodes\NodeInterface;
+
+/**
+ * PAGO30: En un documento relacionado, el importe pagado es requerido cuando
+ *         el tipo de cambio existe o existe más de un documento relacionado (CRP235)
+ */
+class ImportePagadoRequerido extends AbstractDoctoRelacionadoValidator
+{
+    protected $code = 'PAGO30';
+    protected $title = 'En un documento relacionado, el importe pagado es requerido cuando'
+        . ' el tipo de cambio existe o existe más de un documento relacionado (CRP235)';
+
+    public function validateDoctoRelacionado(NodeInterface $docto): bool
+    {
+        if (! isset($docto['ImpPagado'])) {
+            $documentsCount = $this->getPago()->searchNodes('pago10:DoctoRelacionado')->count();
+            if ($documentsCount > 1) {
+                throw $this->exception('No hay importe pagado y hay más de 1 documento en el pago');
+            }
+            if (isset($docto['TipoCambioDR'])) {
+                throw $this->exception('No hay importe pagado y existe el tipo de cambio del documento');
+            }
+        }
+
+        return true;
+    }
+}
