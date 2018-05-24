14aaa31038a908cc6cf1c7a87d17cdc4a5c94c94
Carlos C Soto <eclipxe13@gmail.com>
2018-05-24 14:28:18 -0500

    Add pagos validator Conceptos

---
+++ tests/CfdiUtilsTests/Validate/Cfdi33/RecepcionPagos/ConceptosTest.php
@@ -0,0 +1,154 @@
+<?php
+namespace CfdiUtilsTests\Validate\Cfdi33\RecepcionPagos;
+
+use CfdiUtils\Elements\Cfdi33\Concepto;
+use CfdiUtils\Nodes\Node;
+use CfdiUtils\Validate\Cfdi33\RecepcionPagos\Conceptos;
+use CfdiUtils\Validate\Status;
+
+class ConceptosTest extends ValidateComplementoPagosTestCase
+{
+    /** @var Conceptos */
+    protected $validator;
+
+    /** @var Concepto */
+    protected $concepto;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->validator = new Conceptos();
+
+        // setup a valid case and in the test change to make it fail
+        $comprobante = $this->getComprobante();
+        $this->concepto = $comprobante->addConcepto([
+            'ClaveProdServ' => Conceptos::REQUIRED_CLAVEPRODSERV,
+            'ClaveUnidad' => Conceptos::REQUIRED_CLAVEUNIDAD,
+            'Descripcion' => Conceptos::REQUIRED_DESCRIPCION,
+            'Cantidad' => Conceptos::REQUIRED_CANTIDAD,
+            'ValorUnitario' => Conceptos::REQUIRED_VALORUNITARIO,
+            'Importe' => Conceptos::REQUIRED_IMPORTE,
+        ]);
+    }
+
+    public function testValidCase()
+    {
+        $this->runValidate();
+
+        $this->assertStatusEqualsCode(Status::ok(), 'PAGCON01');
+    }
+
+    public function testConceptosNotExists()
+    {
+        $comprobante = $this->getComprobante();
+        $comprobante->children()->remove($comprobante->getConceptos());
+
+        $this->runValidate();
+
+        $assert = $this->getAssertByCodeOrFail('PAGCON01');
+        $this->assertStatusEqualsAssert(Status::error(), $assert);
+        $this->assertContains('No se encontró el nodo Conceptos', $assert->getExplanation());
+    }
+
+    public function testConceptosZeroChildren()
+    {
+        $comprobante = $this->getComprobante();
+        $comprobante->getConceptos()->children()->removeAll();
+
+        $this->runValidate();
+
+        $assert = $this->getAssertByCodeOrFail('PAGCON01');
+        $this->assertStatusEqualsAssert(Status::error(), $assert);
+        $this->assertContains('Se esperaba encontrar un solo hijo de conceptos', $assert->getExplanation());
+    }
+
+    public function testConceptosChildrenMoreThanOne()
+    {
+        $comprobante = $this->getComprobante();
+        $comprobante->addConcepto();
+
+        $this->runValidate();
+
+        $assert = $this->getAssertByCodeOrFail('PAGCON01');
+        $this->assertStatusEqualsAssert(Status::error(), $assert);
+        $this->assertContains('Se esperaba encontrar un solo hijo de conceptos', $assert->getExplanation());
+    }
+
+    public function testConceptosChildIsNotConcepto()
+    {
+        $comprobante = $this->getComprobante();
+        $conceptos = $comprobante->getConceptos();
+        $conceptos->children()->removeAll();
+        $conceptos->addChild(new Node('cfdi:foo'));
+
+        $this->runValidate();
+
+        $assert = $this->getAssertByCodeOrFail('PAGCON01');
+        $this->assertStatusEqualsAssert(Status::error(), $assert);
+        $this->assertContains('No se encontró el nodo Concepto', $assert->getExplanation());
+    }
+
+    public function testConceptoWithChildren()
+    {
+        $this->concepto->addChild(new Node('cfdi:foo'));
+
+        $this->runValidate();
+
+        $assert = $this->getAssertByCodeOrFail('PAGCON01');
+        $this->assertStatusEqualsAssert(Status::error(), $assert);
+        $this->assertContains('Se esperaba encontrar ningún hijo de concepto', $assert->getExplanation());
+    }
+
+    public function providerConceptoInvalidData()
+    {
+        $second = [
+            [null],
+            [''],
+            ['_'],
+        ];
+        return static::providerFullJoin([
+            ['ClaveProdServ'],
+            ['Cantidad'],
+            ['ClaveUnidad'],
+            ['Descripcion'],
+            ['ValorUnitario'],
+            ['Importe'],
+        ], $second);
+    }
+
+    /**
+     * @param string $attribute
+     * @param string|null $value
+     * @dataProvider providerConceptoInvalidData
+     */
+    public function testConceptoInvalidData(string $attribute, $value)
+    {
+        $this->concepto[$attribute] = $value;
+
+        $this->runValidate();
+
+        $this->assertStatusEqualsCode(Status::error(), 'PAGCON01');
+    }
+
+    public function providerConceptoInvalidDataMustNotExists()
+    {
+        return [
+            ['NoIdentificacion'],
+            ['Unidad'],
+            ['Descuento'],
+        ];
+    }
+
+    /**
+     * @param string $attribute
+     * @dataProvider providerConceptoInvalidDataMustNotExists
+     */
+    public function testConceptoInvalidDataMustNotExists(string $attribute)
+    {
+        $this->concepto[$attribute] = '';
+
+        $this->runValidate();
+
+        $this->assertStatusEqualsCode(Status::error(), 'PAGCON01');
+    }
+}
