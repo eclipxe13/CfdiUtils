421583b33130e315e5b8121e0e3ed702d0fff28e
Carlos C Soto <eclipxe13@gmail.com>
2018-05-24 17:20:51 -0500

    Add pagos/pago validator Fecha

---
+++ src/CfdiUtils/Validate/Cfdi33/RecepcionPagos/Pagos/Fecha.php
@@ -0,0 +1,23 @@
+<?php
+namespace CfdiUtils\Validate\Cfdi33\RecepcionPagos\Pagos;
+
+use CfdiUtils\Nodes\NodeInterface;
+use CfdiUtils\Validate\Cfdi33\Utils\AssertFechaFormat;
+
+/**
+ * PAGO02: En un pago, la fecha debe cumplir con el formato específico
+ */
+class Fecha extends AbstractPagoValidator
+{
+    protected $code = 'PAGO02';
+    protected $title = 'En un pago, la fecha debe cumplir con el formato específico';
+
+    public function validatePago(NodeInterface $pago): bool
+    {
+        if (! AssertFechaFormat::hasFormat($pago['FechaPago'])) {
+            throw new ValidatePagoException(sprintf('FechaPago: "%s"', $pago['FechaPago']));
+        }
+
+        return true;
+    }
+}
