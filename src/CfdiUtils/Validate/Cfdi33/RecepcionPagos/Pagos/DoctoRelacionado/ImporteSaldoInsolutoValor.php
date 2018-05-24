d2e9e491396e6b9b67817de398a53ac69888a934
Carlos C Soto <eclipxe13@gmail.com>
2018-05-24 17:11:58 -0500

    Add pagos/pago/doctorelacionado validator ImporteSaldoInsolutoValor

---
+++ src/CfdiUtils/Validate/Cfdi33/RecepcionPagos/Pagos/DoctoRelacionado/ImporteSaldoInsolutoValor.php
@@ -0,0 +1,35 @@
+<?php
+namespace CfdiUtils\Validate\Cfdi33\RecepcionPagos\Pagos\DoctoRelacionado;
+
+use CfdiUtils\Nodes\NodeInterface;
+use CfdiUtils\Validate\Cfdi33\RecepcionPagos\Helpers\CalculateDocumentAmountTrait;
+
+/**
+ * PAGO28: En un documento relacionado, el importe del saldo insoluto debe ser mayor o igual a cero
+ *         e igual a la resta del importe del saldo anterior menos el importe pagado (CRP226)
+ */
+class ImporteSaldoInsolutoValor extends AbstractDoctoRelacionadoValidator
+{
+    use CalculateDocumentAmountTrait;
+
+    protected $code = 'PAGO28';
+    protected $title = 'En un documento relacionado, el importe del saldo insoluto debe ser mayor o igual a cero'
+        . ' e igual a la resta del importe del saldo anterior menos el importe pagado (CRP226)';
+
+    public function validateDoctoRelacionado(NodeInterface $docto): bool
+    {
+        $value = (float) $docto['ImpSaldoInsoluto'];
+        if (! $this->isEqual(0, $value) && ! $this->isGreaterThan($value, 0)) {
+            throw $this->exception(sprintf('ImpSaldoInsoluto: "%s"', $docto['ImpSaldoInsoluto']));
+        }
+
+        $expected = (float) $docto['ImpSaldoAnt'] - $this->calculateDocumentAmount($docto, $this->getPago());
+        if (! $this->isEqual($value, $expected)) {
+            throw $this->exception(
+                sprintf('ImpSaldoInsoluto: "%s", Esperado: %F', $docto['ImpSaldoInsoluto'], $expected)
+            );
+        }
+
+        return true;
+    }
+}
---
+++ tests/CfdiUtilsTests/Validate/Cfdi33/RecepcionPagos/Pagos/DoctoRelacionado/ImporteSaldoInsolutoValorTest.php
@@ -0,0 +1,76 @@
+<?php
+namespace CfdiUtilsTests\Validate\Cfdi33\RecepcionPagos\Pagos\DoctoRelacionado;
+
+use CfdiUtils\Elements\Pagos10\Pago;
+use CfdiUtils\Validate\Cfdi33\RecepcionPagos\Pagos\DoctoRelacionado\ImporteSaldoInsolutoValor;
+use CfdiUtils\Validate\Cfdi33\RecepcionPagos\Pagos\DoctoRelacionado\ValidateDoctoException;
+use PHPUnit\Framework\TestCase;
+
+class ImporteSaldoInsolutoValorTest extends TestCase
+{
+    /**
+     * @param string $previous
+     * @param string $payment
+     * @param string $left
+     * @testWith ["100.00", "100.00", "0.0"]
+     */
+    public function testValid($previous, $payment, $left)
+    {
+        $pago = new Pago();
+        $docto = $pago->addDoctoRelacionado([
+            'ImpSaldoAnt' => $previous,
+            'ImpPagado' => $payment,
+            'ImpSaldoInsoluto' => $left,
+        ]);
+        $validator = new ImporteSaldoInsolutoValor();
+        $validator->setIndex(0);
+        $validator->setPago($pago);
+
+        $this->assertTrue($validator->validateDoctoRelacionado($docto));
+    }
+
+    /**
+     * @param string $previous
+     * @param string $payment
+     * @param string $left
+     * @testWith ["150.00", "100.00", "50.0"]
+     */
+    public function testWithCalculate($previous, $payment, $left)
+    {
+        $pago = new Pago(['Monto' => $payment]);
+        $docto = $pago->addDoctoRelacionado([
+            'ImpSaldoAnt' => $previous,
+            'ImpSaldoInsoluto' => $left,
+        ]);
+        $validator = new ImporteSaldoInsolutoValor();
+        $validator->setIndex(0);
+        $validator->setPago($pago);
+
+        $this->assertTrue($validator->validateDoctoRelacionado($docto));
+    }
+
+    /**
+     * @param string $previous
+     * @param string $payment
+     * @param string $left
+     * @testWith ["100.00", "100.00", "0.01"]
+     *           ["100.00", "100.00", "-0.01"]
+     *           ["100.01", "100.00", "0.00"]
+     *           ["100.00", "100.01", "0.00"]
+     */
+    public function testInvalid($previous, $payment, $left)
+    {
+        $pago = new Pago();
+        $docto = $pago->addDoctoRelacionado([
+            'ImpSaldoAnt' => $previous,
+            'ImpPagado' => $payment,
+            'ImpSaldoInsoluto' => $left,
+        ]);
+        $validator = new ImporteSaldoInsolutoValor();
+        $validator->setIndex(0);
+        $validator->setPago($pago);
+
+        $this->expectException(ValidateDoctoException::class);
+        $validator->validateDoctoRelacionado($docto);
+    }
+}
