4cffb51bd56ad6cf1b42dc18fface48a7de69a10
Carlos C Soto <eclipxe13@gmail.com>
2018-05-24 14:28:52 -0500

    Add pagos validator UsoCfdi

---
+++ src/CfdiUtils/Validate/Cfdi33/RecepcionPagos/UsoCfdi.php
@@ -0,0 +1,30 @@
+<?php
+namespace CfdiUtils\Validate\Cfdi33\RecepcionPagos;
+
+use CfdiUtils\Nodes\NodeInterface;
+use CfdiUtils\Validate\Asserts;
+use CfdiUtils\Validate\Cfdi33\Abstracts\AbstractRecepcionPagos10;
+use CfdiUtils\Validate\Status;
+
+/**
+ * UsoCfdi
+ *
+ * - PAGUSO01: El uso del CFDI debe ser "P01" (CRP110)
+ */
+class UsoCfdi extends AbstractRecepcionPagos10
+{
+    public function validateRecepcionPagos(NodeInterface $comprobante, Asserts $asserts)
+    {
+        $assert = $asserts->put('PAGUSO01', 'El uso del CFDI debe ser "P01" (CRP110)');
+
+        $receptor = $comprobante->searchNode('cfdi:Receptor');
+        if (null === $receptor) {
+            $assert->setStatus(Status::error(), 'No se encontró el nodo Receptor');
+            return;
+        }
+        $assert->setStatus(
+            Status::when('P01' === $receptor['UsoCFDI']),
+            sprintf('Uso CFDI: "%s"', $receptor['UsoCFDI'])
+        );
+    }
+}
---
+++ tests/CfdiUtilsTests/Validate/Cfdi33/RecepcionPagos/UsoCfdiTest.php
@@ -0,0 +1,44 @@
+<?php
+namespace CfdiUtilsTests\Validate\Cfdi33\RecepcionPagos;
+
+use CfdiUtils\Validate\Cfdi33\RecepcionPagos\UsoCfdi;
+use CfdiUtils\Validate\Status;
+
+class UsoCfdiTest extends ValidateComplementoPagosTestCase
+{
+    /** @var UsoCfdi */
+    protected $validator;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->validator = new UsoCfdi();
+    }
+
+    public function testValidCase()
+    {
+        $comprobante = $this->getComprobante();
+        $comprobante->addReceptor(['UsoCFDI' => 'P01']);
+
+        $this->runValidate();
+
+        $this->assertStatusEqualsCode(Status::ok(), 'PAGUSO01');
+    }
+
+    public function testInvalidUsoCfdi()
+    {
+        $comprobante = $this->getComprobante();
+        $comprobante->addReceptor(['UsoCFDI' => 'P02']);
+
+        $this->runValidate();
+
+        $this->assertStatusEqualsCode(Status::error(), 'PAGUSO01');
+    }
+
+    public function testInvalidNoReceptor()
+    {
+        $this->runValidate();
+
+        $this->assertStatusEqualsCode(Status::error(), 'PAGUSO01');
+    }
+}
