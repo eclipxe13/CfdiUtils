a37928e021dba4c6eca66b70aee6c2bd4aecd722
Carlos C Soto <eclipxe13@gmail.com>
2018-05-24 18:28:17 -0500

    Add pagos/pago validator TipoCambioExists

---
+++ src/CfdiUtils/Validate/Cfdi33/RecepcionPagos/Pagos/TipoCambioExists.php
@@ -0,0 +1,26 @@
+<?php
+namespace CfdiUtils\Validate\Cfdi33\RecepcionPagos\Pagos;
+
+use CfdiUtils\Nodes\NodeInterface;
+
+/**
+ * PAGO05: En un pago, cuando la moneda no es "MXN" no debe existir tipo de cambio,
+ *         de lo contrario el tipo de cambio debe existir (CRP203, CRP204)
+ */
+class TipoCambioExists extends AbstractPagoValidator
+{
+    protected $code = 'PAGO05';
+    protected $title = 'En un pago, cuando la moneda no es "MXN" no debe existir tipo de cambio,'
+        . ' de lo contrario el tipo de cambio debe existir (CRP203, CRP204)';
+
+    public function validatePago(NodeInterface $pago): bool
+    {
+        if (! (('MXN' === $pago['MonedaP']) xor ('' !== $pago['TipoCambioP']))) {
+            throw new ValidatePagoException(
+                sprintf('Moneda: "%s", Tipo de cambio: "%s"', $pago['MonedaP'], $pago['TipoCambioP'])
+            );
+        }
+
+        return true;
+    }
+}
