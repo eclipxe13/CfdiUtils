b07eb13749704597bc6a9125d724c2b55f253e1b
Carlos C Soto <eclipxe13@gmail.com>
2018-05-24 17:12:42 -0500

    Add pagos/pago/doctorelacionado validator NumeroParcialidadRequerido

---
+++ src/CfdiUtils/Validate/Cfdi33/RecepcionPagos/Pagos/DoctoRelacionado/NumeroParcialidadRequerido.php
@@ -0,0 +1,24 @@
+<?php
+namespace CfdiUtils\Validate\Cfdi33\RecepcionPagos\Pagos\DoctoRelacionado;
+
+use CfdiUtils\Nodes\NodeInterface;
+
+/**
+ * PAGO31: En un documento relacionado, el número de parcialidad es requerido cuando
+ *         el tipo de cambio existe o existe más de un documento relacionado (CRP234)
+ */
+class NumeroParcialidadRequerido extends AbstractDoctoRelacionadoValidator
+{
+    protected $code = 'PAGO31';
+    protected $title = 'En un documento relacionado, el número de parcialidad es requerido cuando'
+        . ' el tipo de cambio existe o existe más de un documento relacionado (CRP233)';
+
+    public function validateDoctoRelacionado(NodeInterface $docto): bool
+    {
+        if (! isset($docto['NumParcialidad']) && 'PPD' === $docto['MetodoDePagoDR']) {
+            throw $this->exception('No hay número de parcialidad y el método de pago es PPD');
+        }
+
+        return true;
+    }
+}
