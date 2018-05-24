ba2b6e8a8524b6d866e3583893e8ef6d8e198a55
Carlos C Soto <eclipxe13@gmail.com>
2018-05-24 17:19:31 -0500

    Add pagos/pago validator CuentaOrdenantePatron

---
+++ tests/CfdiUtilsTests/Validate/Cfdi33/RecepcionPagos/Pagos/CuentaOrdenantePatronTest.php
@@ -0,0 +1,42 @@
+<?php
+namespace CfdiUtilsTests\Validate\Cfdi33\RecepcionPagos\Pagos;
+
+use CfdiUtils\Elements\Pagos10\Pago;
+use CfdiUtils\Validate\Cfdi33\RecepcionPagos\Pagos\CuentaOrdenantePatron;
+use CfdiUtils\Validate\Cfdi33\RecepcionPagos\Pagos\ValidatePagoException;
+use PHPUnit\Framework\TestCase;
+
+class CuentaOrdenantePatronTest extends TestCase
+{
+    /**
+     * @param string $input
+     * @testWith ["1234567890123456"]
+     *           [null]
+     */
+    public function testValid($input)
+    {
+        $pago = new Pago([
+            'FormaDePagoP' => '04', // require a pattern of 16 digits
+            'CtaOrdenante' => $input,
+        ]);
+        $validator = new CuentaOrdenantePatron();
+        $this->assertTrue($validator->validatePago($pago));
+    }
+
+    /**
+     * @param string $input
+     * @testWith ["1"]
+     *           [""]
+     */
+    public function testInvalid($input)
+    {
+        $pago = new Pago([
+            'FormaDePagoP' => '04', // require a pattern of 16 digits
+            'CtaOrdenante' => $input,
+        ]);
+        $validator = new CuentaOrdenantePatron();
+
+        $this->expectException(ValidatePagoException::class);
+        $validator->validatePago($pago);
+    }
+}
