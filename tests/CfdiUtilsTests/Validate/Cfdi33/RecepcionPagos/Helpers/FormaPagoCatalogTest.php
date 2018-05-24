f8e2574ef306fcd67432efae41c28c839d29f27e
Carlos C Soto <eclipxe13@gmail.com>
2018-05-24 17:02:49 -0500

    Add helpers FormaPagoEntry and FormaPagoCatalog

---
+++ tests/CfdiUtilsTests/Validate/Cfdi33/RecepcionPagos/Helpers/FormaPagoCatalogTest.php
@@ -0,0 +1,51 @@
+<?php
+namespace CfdiUtilsTests\Validate\Cfdi33\RecepcionPagos\Helpers;
+
+use CfdiUtils\Validate\Cfdi33\RecepcionPagos\Helpers\FormaPagoCatalog;
+use PHPUnit\Framework\TestCase;
+
+class FormaPagoCatalogTest extends TestCase
+{
+    public function providerObtain()
+    {
+        return [
+            'Efectivo' => ['01'],
+            'Cheque nominativo' => ['02'],
+            'Transferencia electrónica de fondos' => ['03'],
+            'Tarjeta de crédito' => ['04'],
+            'Monedero electrónico' => ['05'],
+            'Dinero electrónico' => ['06'],
+            'Vales de despensa' => ['08'],
+            'Dación en pago' => ['12'],
+            'Pago por subrogación' => ['13'],
+            'Pago por consignación' => ['14'],
+            'Condonación' => ['15'],
+            'Compensación' => ['17'],
+            'Novación' => ['23'],
+            'Confusión' => ['24'],
+            'Remisión de deuda' => ['25'],
+            'Prescripción o caducidad' => ['26'],
+            'A satisfacción del acreedor' => ['27'],
+            'Tarjeta de débito' => ['28'],
+            'Tarjeta de servicios' => ['29'],
+            'Por definir' => ['99'],
+        ];
+    }
+
+    /**
+     * @param string $key
+     * @dataProvider providerObtain
+     */
+    public function testObtain(string $key)
+    {
+        $paymentType = (new FormaPagoCatalog())->obtain($key);
+        $this->assertSame($key, $paymentType->key());
+    }
+
+    public function testObtainWithNonExistentKey()
+    {
+        $this->expectException(\OutOfBoundsException::class);
+        $this->expectExceptionMessage('FOO');
+        (new FormaPagoCatalog())->obtain('FOO');
+    }
+}
