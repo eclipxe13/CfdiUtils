d9e2dc24089d33bf2fb0524ff8ef2fd86ee56b0e
Carlos C Soto <eclipxe13@gmail.com>
2018-05-24 18:26:10 -0500

    Add pagos/pago validator TipoCadenaPagoCadena

---
+++ src/CfdiUtils/Validate/Cfdi33/RecepcionPagos/Pagos/TipoCadenaPagoCadena.php
@@ -0,0 +1,27 @@
+<?php
+namespace CfdiUtils\Validate\Cfdi33\RecepcionPagos\Pagos;
+
+use CfdiUtils\Nodes\NodeInterface;
+
+/**
+ * PAGO21: En un pago, si existe el tipo de cadena de pago debe existir
+ *         la cadena del pago y viceversa (CRP229 y CRP230)
+ */
+class TipoCadenaPagoCadena extends AbstractPagoValidator
+{
+    protected $code = 'PAGO21';
+    protected $title = 'En un pago, si existe el tipo de cadena de pago debe existir'
+        . ' la cadena del pago y viceversa (CRP229 y CRP230)';
+
+    public function validatePago(NodeInterface $pago): bool
+    {
+        if ((('' !== $pago['TipoCadPago']) xor ('' !== $pago['CadPago']))
+            || (isset($pago['TipoCadPago']) xor isset($pago['CadPago']))) {
+            throw new ValidatePagoException(
+                sprintf('Tipo cadena pago: "%s", Cadena: "%s"', $pago['TipoCadPago'], $pago['CadPago'])
+            );
+        }
+
+        return true;
+    }
+}
