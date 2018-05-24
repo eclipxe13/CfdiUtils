22322ee5b80fc1590a303ff48429544d3f524f18
Carlos C Soto <eclipxe13@gmail.com>
2018-05-24 17:32:26 -0500

    Add pagos/pago validator MontoDecimals

---
+++ tests/CfdiUtilsTests/Validate/Cfdi33/RecepcionPagos/Pagos/MontoDecimalsTest.php
@@ -0,0 +1,39 @@
+<?php
+namespace CfdiUtilsTests\Validate\Cfdi33\RecepcionPagos\Pagos;
+
+use CfdiUtils\Elements\Pagos10\Pago;
+use CfdiUtils\Validate\Cfdi33\RecepcionPagos\Pagos\MontoDecimals;
+use CfdiUtils\Validate\Cfdi33\RecepcionPagos\Pagos\ValidatePagoException;
+use PHPUnit\Framework\TestCase;
+
+class MontoDecimalsTest extends TestCase
+{
+    public function testValid()
+    {
+        $pago = new Pago([
+            'MonedaP' => 'USD', // 2 decimals
+            'Monto' => '1.00',
+        ]);
+        $validator = new MontoDecimals();
+
+        $this->assertTrue($validator->validatePago($pago));
+    }
+
+    /**
+     * @param string|null $amount
+     * @testWith ["0.001"]
+     *           ["0.000"]
+     *           ["0.123"]
+     */
+    public function testInvalid($amount)
+    {
+        $pago = new Pago([
+            'MonedaP' => 'USD', // 2 decimals
+            'Monto' => $amount,
+        ]);
+        $validator = new MontoDecimals();
+
+        $this->expectException(ValidatePagoException::class);
+        $validator->validatePago($pago);
+    }
+}
