e5aa7f6548327e18123cb7733853ce6d1df44688
Carlos C Soto <eclipxe13@gmail.com>
2018-05-24 19:57:53 -0500

    Move version logic to a new abstract class VersionDiscoverer

---
+++ src/CfdiUtils/VersionDiscovery/NodeContainer.php
@@ -0,0 +1,20 @@
+<?php
+namespace CfdiUtils\VersionDiscovery;
+
+use CfdiUtils\Nodes\NodeInterface;
+
+class NodeContainer implements ContainerWithAttributeInterface
+{
+    /** @var NodeInterface */
+    private $node;
+
+    public function __construct(NodeInterface $node)
+    {
+        $this->node = $node;
+    }
+
+    public function getAttributeValue(string $attribute): string
+    {
+        return $this->node[$attribute];
+    }
+}
