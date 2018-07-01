ad1e2510eacf8a5c5a89feaf2777d8f5a97e5343
Cesar Aguilera <cesargnu29@gmail.com>
2018-07-01 00:47:52 -0500

    Add method to string

---
+++ tests/CfdiUtilsTests/CfdiCreatorToStringTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace CfdiUtilsTests;
+
+use CfdiUtils\CfdiCreator33;
+
+class CfdiCreatorToStringTest extends TestCase
+{
+    public function testWhenCastingToStringWithExceptionOnlyReturnsAnEmptyString()
+    {
+        /** @var CfdiCreator33|\PHPunit\Framework\MockObject\MockObject $cfdiCreator */
+        $cfdiCreator = $this->$this->getMockBuilder(CfdiCreator33::class)
+            ->setMethods(['asXml'])
+            ->getMock();
+
+        $cfdiCreator->method('asXml')->willThrowException(new \RuntimeException('exception'));
+
+        $this->assertSame('', (string)$cfdiCreator);
+    }
+
+    public function testCastToStringReturnAValidXml()
+    {
+        $cfdiCreator = new CfdiCreator33();
+        $xml = $cfdiCreator->asXml();
+        $this->assertNotEmpty($xml);
+        $this->assertSame($xml, (string)$cfdiCreator);
+    }
+}
