ad478ca5448b17273b62dc31c3cabf04401548d2
Carlos C Soto <eclipxe13@gmail.com>
2018-10-04 19:52:07 -0500

    Create tests for trait `CalculateDocumentAmountTrait`

---
+++ tests/CfdiUtilsTests/Validate/Cfdi33/RecepcionPagos/Helpers/CalculateDocumentAmountUse.php
@@ -0,0 +1,9 @@
+<?php
+namespace CfdiUtilsTests\Validate\Cfdi33\RecepcionPagos\Helpers;
+
+use CfdiUtils\Validate\Cfdi33\RecepcionPagos\Helpers\CalculateDocumentAmountTrait;
+
+class CalculateDocumentAmountUse
+{
+    use CalculateDocumentAmountTrait;
+}
