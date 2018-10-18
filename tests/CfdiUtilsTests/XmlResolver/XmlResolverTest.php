ae2230baee72e70393764d62f30a7ec77d47e68f
Carlos C Soto <eclipxe13@gmail.com>
2018-10-17 21:23:41 -0500

    Remove owner information

--- tests/CfdiUtilsTests/XmlResolver/XmlResolverTest.php
+++ tests/CfdiUtilsTests/XmlResolver/XmlResolverTest.php
@@ -1,15 +1,4 @@
 <?php
-/*
- * This file is part of the eclipxe/cfdi library.
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- *
- * @copyright Copyright (c) Carlos C Soto <eclipxe13@gmail.com>
- * @license http://opensource.org/licenses/MIT MIT
- * @link https://github.com/eclipxe13/cfdi GitHub
- * @link https://github.com/orlandocharles/cfdi Original project
- */
 namespace CfdiUtilsTests\XmlResolver;
 
 use CfdiUtils\Certificado\SatCertificateNumber;
