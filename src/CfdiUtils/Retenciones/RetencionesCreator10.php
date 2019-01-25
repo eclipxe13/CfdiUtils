3000bf9f35a93c2c4140a8c417d0dc72e28c00df
Carlos C Soto <eclipxe13@gmail.com>
2019-01-25 13:34:19 -0600

    RetencionesCreator10 uses Element\Retenciones10\Retenciones helper

--- src/CfdiUtils/Retenciones/RetencionesCreator10.php
+++ src/CfdiUtils/Retenciones/RetencionesCreator10.php
@@ -8,8 +8,7 @@ use CfdiUtils\CadenaOrigen\XsltBuilderPropertyTrait;
 use CfdiUtils\Certificado\Certificado;
 use CfdiUtils\Certificado\CertificadoPropertyInterface;
 use CfdiUtils\Certificado\CertificadoPropertyTrait;
-use CfdiUtils\Nodes\Node;
-use CfdiUtils\Nodes\NodeInterface;
+use CfdiUtils\Elements\Retenciones10\Retenciones;
 use CfdiUtils\Nodes\XmlNodeUtils;
 use CfdiUtils\PemPrivateKey\PemPrivateKey;
 use CfdiUtils\Validate\Asserts;
@@ -18,7 +17,7 @@ use CfdiUtils\XmlResolver\XmlResolver;
 use CfdiUtils\XmlResolver\XmlResolverPropertyInterface;
 use CfdiUtils\XmlResolver\XmlResolverPropertyTrait;
 
-class Retenciones10 implements
+class RetencionesCreator10 implements
     CertificadoPropertyInterface,
     XmlResolverPropertyInterface,
     XsltBuilderPropertyInterface
@@ -30,45 +29,31 @@ class Retenciones10 implements
     const XSD_1_0 = 'http://www.sat.gob.mx/esquemas/retencionpago/1/retencionpagov1.xsd';
     const XMLNS_1_0 = 'http://www.sat.gob.mx/esquemas/retencionpago/1';
 
-    /** @var NodeInterface */
-    private $root;
+    /** @var Retenciones */
+    private $retenciones;
 
-    /**
-     * Retenciones10 constructor.
-     * @param array $comprobanteAttributes
-     * @param NodeInterface[] $children
-     * @param XmlResolver|null $xmlResolver
-     * @param XsltBuilderInterface|null $xsltBuilder
-     */
     public function __construct(
         array $comprobanteAttributes = [],
-        array $children = [],
         XmlResolver $xmlResolver = null,
         XsltBuilderInterface $xsltBuilder = null
     ) {
-        $this->root = new Node('retenciones:Retenciones', array_merge($comprobanteAttributes, [
-            'xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance',
-            'xmlns:retenciones' => self::XMLNS_1_0,
-            'xsi:schemaLocation' => self::XMLNS_1_0 . ' ' . self::XSD_1_0,
-            'Version' => '1.0',
-        ]));
-        $this->root->children()->add(...$children);
+        $this->retenciones = new Retenciones($comprobanteAttributes);
         $this->setXmlResolver($xmlResolver ? : new XmlResolver());
         $this->setXsltBuilder($xsltBuilder ? : new DOMBuilder());
     }
 
-    public function rootNode(): NodeInterface
+    public function retenciones(): Retenciones
     {
-        return $this->root;
+        return $this->retenciones;
     }
 
     public function putCertificado(Certificado $certificado)
     {
         $this->setCertificado($certificado);
         $cerfile = $certificado->getFilename();
-        $this->root['NumCert'] = $certificado->getSerial();
+        $this->retenciones['NumCert'] = $certificado->getSerial();
         if (file_exists($cerfile)) {
-            $this->root['Cert'] = base64_encode((string) file_get_contents($cerfile));
+            $this->retenciones['Cert'] = base64_encode((string) file_get_contents($cerfile));
         }
     }
 
@@ -98,7 +83,7 @@ class Retenciones10 implements
         }
 
         // create sign and set into Sello attribute
-        $this->root['Sello'] = base64_encode(
+        $this->retenciones['Sello'] = base64_encode(
             $privateKey->sign($this->buildCadenaDeOrigen(), OPENSSL_ALGO_SHA1)
         );
     }
@@ -108,12 +93,12 @@ class Retenciones10 implements
         $validator = new XmlFollowSchema();
         $validator->setXmlResolver($this->getXmlResolver());
         $asserts = new Asserts();
-        $validator->validate($this->root, $asserts);
+        $validator->validate($this->retenciones, $asserts);
         return $asserts;
     }
 
     public function asXml(): string
     {
-        return XmlNodeUtils::nodeToXmlString($this->root, true);
+        return XmlNodeUtils::nodeToXmlString($this->retenciones, true);
     }
 }
--- tests/CfdiUtilsTests/Retenciones/Retenciones10Test.php
+++ tests/CfdiUtilsTests/Retenciones/Retenciones10Test.php
@@ -4,7 +4,7 @@ namespace CfdiUtilsTests\Retenciones;
 use CfdiUtils\CadenaOrigen\DOMBuilder;
 use CfdiUtils\Certificado\Certificado;
 use CfdiUtils\Nodes\Node;
-use CfdiUtils\Retenciones\Retenciones10;
+use CfdiUtils\Retenciones\RetencionesCreator10;
 use CfdiUtilsTests\TestCase;
 
 class Retenciones10Test extends TestCase
@@ -19,87 +19,83 @@ class Retenciones10Test extends TestCase
         $xsltBuilder = new DOMBuilder();
 
         // create object
-        $retencion = new Retenciones10([
+        $creator = new RetencionesCreator10([
             'FechaExp' => '2019-01-23T08:00:00-06:00',
             'CveRetenc' => '14', // Dividendos o utilidades distribuidos
-        ], [
-            new Node('retenciones:Emisor', [
-                'RFCEmisor' => 'AAA010101AAA',
-                'NomDenRazSocE' => 'ACCEM SERVICIOS EMPRESARIALES SC',
-            ]),
-            new Node('retenciones:Receptor', [
-                'Nacionalidad' => 'Extranjero',
-            ], [
-                new Node('retenciones:Extranjero', [
-                    'NumRegIdTrib' => '998877665544332211',
-                    'NomDenRazSocR' => 'WORLD WIDE COMPANY INC',
-                ]),
-            ]),
-            new Node('retenciones:Periodo', ['MesIni' => '5', 'MesFin' => '5', 'Ejerc' => '2018']),
-            new Node('retenciones:Totales', [
-                'montoTotOperacion' => '55578643',
-                'montoTotGrav' => '0',
-                'montoTotExent' => '55578643',
-                'montoTotRet' => '0',
-            ], [
-                new Node('retenciones:ImpRetenidos', [
-                    'BaseRet' => '0',
-                    'Impuesto' => '01', // 01 - ISR
-                    'montoRet' => '0',
-                    'TipoPagoRet' => 'Pago provisional',
-                ]),
-            ]),
-            new Node('retenciones:Complemento', [], [
-                new Node('dividendos:Dividendos', [
-                    'xmlns:dividendos' => 'http://www.sat.gob.mx/esquemas/retencionpago/1/dividendos',
-                    'xsi:schemaLocation' => vsprintf('%s %s', [
-                        'http://www.sat.gob.mx/esquemas/retencionpago/1/dividendos',
-                        'http://www.sat.gob.mx/esquemas/retencionpago/1/dividendos/dividendos.xsd',
-                    ]),
-                    'Version' => '1.0',
-                ], [
-                    new Node('dividendos:DividOUtil', [
-                        'CveTipDivOUtil' => '06', // 06 - Proviene de CUFIN al 31 de diciembre 2013
-                        'MontISRAcredRetMexico' => '0',
-                        'MontISRAcredRetExtranjero' => '0',
-                        'MontRetExtDivExt' => '0',
-                        'TipoSocDistrDiv' => 'Sociedad Nacional',
-                        'MontISRAcredNal' => '0',
-                        'MontDivAcumNal' => '0',
-                        'MontDivAcumExt' => '0',
-                    ]),
-                ]),
-            ]),
         ], $xmlResolver, $xsltBuilder);
+        $retenciones = $creator->retenciones();
+        $retenciones->addEmisor([
+            'RFCEmisor' => 'AAA010101AAA',
+            'NomDenRazSocE' => 'ACCEM SERVICIOS EMPRESARIALES SC',
+        ]);
+        $retenciones->getReceptor()->addExtranjero([
+            'NumRegIdTrib' => '998877665544332211',
+            'NomDenRazSocR' => 'WORLD WIDE COMPANY INC',
+        ]);
+        $retenciones->addPeriodo(['MesIni' => '5', 'MesFin' => '5', 'Ejerc' => '2018']);
+        $retenciones->addTotales([
+            'montoTotOperacion' => '55578643',
+            'montoTotGrav' => '0',
+            'montoTotExent' => '55578643',
+            'montoTotRet' => '0',
+        ]);
+        $retenciones->addImpRetenidos([
+            'BaseRet' => '0',
+            'Impuesto' => '01', // 01 - ISR
+            'montoRet' => '0',
+            'TipoPagoRet' => 'Pago provisional',
+        ]);
+
+        $retenciones->addComplemento(
+            new Node('dividendos:Dividendos', [
+                'xmlns:dividendos' => 'http://www.sat.gob.mx/esquemas/retencionpago/1/dividendos',
+                'xsi:schemaLocation' => vsprintf('%s %s', [
+                    'http://www.sat.gob.mx/esquemas/retencionpago/1/dividendos',
+                    'http://www.sat.gob.mx/esquemas/retencionpago/1/dividendos/dividendos.xsd',
+                ]),
+                'Version' => '1.0',
+            ], [
+                new Node('dividendos:DividOUtil', [
+                    'CveTipDivOUtil' => '06', // 06 - Proviene de CUFIN al 31 de diciembre 2013
+                    'MontISRAcredRetMexico' => '0',
+                    'MontISRAcredRetExtranjero' => '0',
+                    'MontRetExtDivExt' => '0',
+                    'TipoSocDistrDiv' => 'Sociedad Nacional',
+                    'MontISRAcredNal' => '0',
+                    'MontDivAcumNal' => '0',
+                    'MontDivAcumExt' => '0',
+                ]),
+            ])
+        );
 
         // verify properties
-        $this->assertSame($xmlResolver, $retencion->getXmlResolver());
-        $this->assertSame($xsltBuilder, $retencion->getXsltBuilder());
+        $this->assertSame($xmlResolver, $creator->getXmlResolver());
+        $this->assertSame($xsltBuilder, $creator->getXsltBuilder());
 
         // verify root node
-        $root = $retencion->rootNode();
-        $this->assertSame(Retenciones10::XMLNS_1_0, $root['xmlns:retenciones']);
-        $this->assertSame(Retenciones10::XMLNS_1_0 . ' ' . Retenciones10::XSD_1_0, $root['xsi:schemaLocation']);
+        $root = $creator->retenciones();
+        $this->assertSame(RetencionesCreator10::XMLNS_1_0, $root['xmlns:retenciones']);
+        $this->assertSame(RetencionesCreator10::XMLNS_1_0 . ' ' . RetencionesCreator10::XSD_1_0, $root['xsi:schemaLocation']);
         $this->assertSame('1.0', $root['Version']);
 
         // put additional content using helpers
-        $retencion->putCertificado($certificado);
-        $retencion->addSello('file://' . $pemFile, $passPhrase);
+        $creator->putCertificado($certificado);
+        $creator->addSello('file://' . $pemFile, $passPhrase);
 
         // validate
-        $asserts = $retencion->validate();
+        $asserts = $creator->validate();
         $this->assertGreaterThanOrEqual(1, $asserts->count());
         $this->assertTrue($asserts->exists('XSD01'));
         $this->assertSame('', $asserts->get('XSD01')->getExplanation());
         $this->assertFalse($asserts->hasErrors());
 
         // check against known content
-        $this->assertXmlStringEqualsXmlFile($this->utilAsset('retenciones/sample-before-tfd.xml'), $retencion->asXml());
+        $this->assertXmlStringEqualsXmlFile($this->utilAsset('retenciones/sample-before-tfd.xml'), $creator->asXml());
     }
 
     public function testValidateIsCheckingAgainstXsdViolations()
     {
-        $retencion = new Retenciones10();
+        $retencion = new RetencionesCreator10();
         $retencion->setXmlResolver($this->newResolver());
         $assert = $retencion->validate()->get('XSD01');
         $this->assertTrue($assert->getStatus()->isError());
@@ -110,7 +106,7 @@ class Retenciones10Test extends TestCase
         $pemFile = $this->utilAsset('certs/CSD01_AAA010101AAA_password.key.pem');
         $passPhrase = '_worng_passphrase_';
 
-        $retencion = new Retenciones10();
+        $retencion = new RetencionesCreator10();
         $retencion->setXmlResolver($this->newResolver());
 
         $this->expectException(\RuntimeException::class);
@@ -125,7 +121,7 @@ class Retenciones10Test extends TestCase
         $passPhrase = '';
         $certificado = new Certificado($cerFile);
 
-        $retencion = new Retenciones10();
+        $retencion = new RetencionesCreator10();
         $retencion->setXmlResolver($this->newResolver());
 
         $retencion->putCertificado($certificado);
