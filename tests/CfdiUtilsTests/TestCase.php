c1001b089dcea2af6e056179979c7aac98d91c28
Carlos C Soto <eclipxe13@gmail.com>
2019-03-15 14:32:54 -0600

    Check DIRECTORY_SEPARATOR instead of PHP_OS (2)

--- tests/CfdiUtilsTests/TestCase.php
+++ tests/CfdiUtilsTests/TestCase.php
@@ -15,7 +15,7 @@ class TestCase extends \PHPUnit\Framework\TestCase
 
     protected function isRunningOnWindows(): bool
     {
-        return ('/' !== DIRECTORY_SEPARATOR);
+        return ('\\' === DIRECTORY_SEPARATOR);
     }
 
     protected function newInsecurePhpDownloader(): DownloaderInterface
