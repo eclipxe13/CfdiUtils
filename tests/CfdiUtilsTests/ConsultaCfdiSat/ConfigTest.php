2c053421e221b9d9feaec86a07279bb00a1230e8
Carlos C Soto <eclipxe13@gmail.com>
2019-03-26 16:54:12 -0600

    Deprecate WSDL configuration

--- tests/CfdiUtilsTests/ConsultaCfdiSat/ConfigTest.php
+++ tests/CfdiUtilsTests/ConsultaCfdiSat/ConfigTest.php
@@ -11,16 +11,14 @@ class ConfigTest extends TestCase
         $config = new Config();
         $this->assertSame(10, $config->getTimeout());
         $this->assertSame(true, $config->shouldVerifyPeer());
-        $this->assertSame($config::DEFAULT_WSDL_URL, $config->getServiceUrl());
-        $this->assertSame($config::DEFAULT_WSDL_URL, $config->getWsdlLocation());
+        $this->assertSame($config::DEFAULT_SERVICE_URL, $config->getServiceUrl());
     }
 
     public function testConstructorWithOtherData()
     {
-        $config = new Config(99, false, 'foo', 'bar');
+        $config = new Config(99, false, 'foo');
         $this->assertSame(99, $config->getTimeout());
         $this->assertSame(false, $config->shouldVerifyPeer());
         $this->assertSame('foo', $config->getServiceUrl());
-        $this->assertSame('bar', $config->getWsdlLocation());
     }
 }
