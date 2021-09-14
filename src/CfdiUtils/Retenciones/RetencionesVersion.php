163cab9710a426d762ce9195fb6ffe3e5dfb50b7
Carlos C Soto <eclipxe13@gmail.com>
2021-09-13 21:55:17 -0500

    Remove CfdiUtils\VersionDiscovery\StaticMethodsCompatTrait

--- src/CfdiUtils/Retenciones/RetencionesVersion.php
+++ src/CfdiUtils/Retenciones/RetencionesVersion.php
@@ -1,25 +1,24 @@
 <?php
 
-namespace CfdiUtils;
+namespace CfdiUtils\Retenciones;
 
 use CfdiUtils\VersionDiscovery\VersionDiscoverer;
 
 /**
  * This class provides the methods to retrieve the version attribute from a
- * Comprobante Fiscal Digital por Internet (CFDI)
+ * CFDI de Retenciones e información de pagos (Retenciones)
  *
  * It will not check anything but the value of the correct attribute
  * It will not care if the element is following a schema or element's name
  *
- * Possible values are always 3.2, 3.3 or empty string
+ * Possible values are always 1.0 or empty string
  */
-class CfdiVersion extends VersionDiscoverer
+final class RetencionesVersion extends VersionDiscoverer
 {
     public function rules(): array
     {
         return [
-            '3.3' => 'Version',
-            '3.2' => 'version',
+            '1.0' => 'Version',
         ];
     }
 }
--- src/CfdiUtils/TimbreFiscalDigital/TfdVersion.php
+++ src/CfdiUtils/TimbreFiscalDigital/TfdVersion.php
@@ -2,27 +2,19 @@
 
 namespace CfdiUtils\TimbreFiscalDigital;
 
-use CfdiUtils\VersionDiscovery\StaticMethodsCompatTrait;
 use CfdiUtils\VersionDiscovery\VersionDiscoverer;
 
 /**
- * This class provides static methods to retrieve the version attribute from a
+ * This class provides the methods to retrieve the version attribute from a
  * Timbre Fiscal Digital (TFD)
  *
  * It will not check anything but the value of the correct attribute
- * It will not care if the cfdi is following an schema or element's name
+ * It will not care if the element is following a schema or element's name
  *
  * Possible values are always 1.0, 1.1 or empty string
  */
 class TfdVersion extends VersionDiscoverer
 {
-    use StaticMethodsCompatTrait;
-
-    protected static function createDiscoverer(): VersionDiscoverer
-    {
-        return new self();
-    }
-
     public function rules(): array
     {
         return [
