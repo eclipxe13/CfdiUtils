184ef6f79bd1ec930a4dfc9e7a29e98acc505414
AndreyPootMay <andreypootmay@gmail.com>
2021-10-26 16:52:31 -0500

    Primer commit, levantando los primeros 4 elementos, donde CartaPorte es el principal mientras que los otros tres son sus primeros hijos

---
+++ src/CfdiUtils/Elements/CartaPorte10/Mercancias.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace CfdiUtils\Elements\CartaPorte10;
+
+use CfdiUtils\Elements\Common\AbstractElement;
+
+class Mercancias extends AbstractElement
+{
+    public function getElementName(): string
+    {
+        return 'cartaporte:Mercancias';
+    }
+}
---
+++ src/CfdiUtils/Elements/CartaPorte10/Ubicaciones.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace CfdiUtils\Elements\CartaPorte10;
+
+use CfdiUtils\Elements\Common\AbstractElement;
+
+class Ubicaciones extends AbstractElement
+{
+    public function getElementName(): string
+    {
+        return 'cartaporte:Ubicaciones';
+    }
+}
