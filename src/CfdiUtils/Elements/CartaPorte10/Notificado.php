b8da23014616657b7fdfca6dec3b6b9e8ccdad7d
celli33 <limon.martinez.misael@gmail.com>
2021-12-24 12:37:52 -0600

    added mising Domicilio helper methods

--- src/CfdiUtils/Elements/CartaPorte10/Notificado.php
+++ src/CfdiUtils/Elements/CartaPorte10/Notificado.php
@@ -10,4 +10,18 @@ class Notificado extends AbstractElement
     {
         return 'cartaporte:Notificado';
     }
+
+    public function getDomicilio(): Domicilio
+    {
+        /** @noinspection PhpIncompatibleReturnTypeInspection */
+        return $this->helperGetOrAdd(new Domicilio());
+    }
+
+    public function addDomicilio(array $attributes = []): Domicilio
+    {
+        $domicilio = $this->getDomicilio();
+        $domicilio->addAttributes($attributes);
+
+        return $domicilio;
+    }
 }
