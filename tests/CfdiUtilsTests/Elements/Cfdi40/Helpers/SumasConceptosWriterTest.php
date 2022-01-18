e468ed5e7283e019884dc70517fbc28c0a77f0d3
Carlos C Soto <eclipxe13@gmail.com>
2022-01-17 19:43:30 -0600

    Test SumasConceptosWriter for Cfdi 3.3 & 4.0

--- tests/CfdiUtilsTests/Elements/Cfdi40/Helpers/SumasConceptosWriterTest.php
+++ tests/CfdiUtilsTests/Elements/Cfdi40/Helpers/SumasConceptosWriterTest.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace CfdiUtilsTests\Elements\Cfdi33\Helpers;
+namespace CfdiUtilsTests\Elements\Cfdi40\Helpers;
 
-use CfdiUtils\Elements\Cfdi33\Comprobante;
-use CfdiUtils\Elements\Cfdi33\Helpers\SumasConceptosWriter;
+use CfdiUtils\Elements\Cfdi40\Comprobante;
+use CfdiUtils\Elements\Cfdi40\Helpers\SumasConceptosWriter;
 use CfdiUtils\SumasConceptos\SumasConceptos;
 use CfdiUtilsTests\SumasConceptos\SumasConceptosWriterTestTrait;
 use PHPUnit\Framework\TestCase;
@@ -28,6 +28,6 @@ final class SumasConceptosWriterTest extends TestCase
         $this->assertSame($precision, $writer->getPrecision());
         $this->assertSame($sumasConceptos, $writer->getSumasConceptos());
         $this->assertSame($precision, $writer->getPrecision());
-        $this->assertSame(false, $writer->hasWriteImpuestoBase());
+        $this->assertSame(true, $writer->hasWriteImpuestoBase());
     }
 }
