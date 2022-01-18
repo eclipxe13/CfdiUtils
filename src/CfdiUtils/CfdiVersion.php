1b5715fff0427a6be2d0219bdb5d12efacef0ea8
Carlos C Soto <eclipxe13@gmail.com>
2022-01-17 22:32:31 -0600

    Update CfdiVersion phpdoc and increase coverage

--- src/CfdiUtils/CfdiVersion.php
+++ src/CfdiUtils/CfdiVersion.php
@@ -10,9 +10,9 @@ use CfdiUtils\VersionDiscovery\VersionDiscoverer;
  * Comprobante Fiscal Digital por Internet (CFDI)
  *
  * It will not check anything but the value of the correct attribute
- * It will not care if the cfdi is following an schema or element's name
+ * It will not care if the cfdi is following a schema or element's name
  *
- * Possible values are always 3.2, 3.3 or empty string
+ * Possible values are always 3.2, 3.3, 4.0 or empty string
  */
 class CfdiVersion extends VersionDiscoverer
 {
--- tests/CfdiUtilsTests/CfdiVersionTest.php
+++ tests/CfdiUtilsTests/CfdiVersionTest.php
@@ -5,9 +5,22 @@ namespace CfdiUtilsTests;
 use CfdiUtils\CfdiVersion;
 use CfdiUtils\Nodes\Node;
 use CfdiUtils\Nodes\XmlNodeUtils;
+use CfdiUtils\VersionDiscovery\VersionDiscoverer;
 
 final class CfdiVersionTest extends TestCase
 {
+    public function testCreateDiscoverer(): void
+    {
+        $extended = new class() extends CfdiVersion {
+            public static function exposeCreateDiscoverer(): VersionDiscoverer
+            {
+                return static::createDiscoverer();
+            }
+        };
+
+        $this->assertInstanceOf(CfdiVersion::class, $extended::exposeCreateDiscoverer());
+    }
+
     public function providerCfdiVersion(): array
     {
         return [
@@ -35,7 +48,7 @@ final class CfdiVersionTest extends TestCase
      * @param string|null $value
      * @dataProvider providerCfdiVersion
      */
-    public function testCfdiVersion(string $expected, string $attribute, ?string $value)
+    public function testCfdiVersion(string $expected, string $attribute, ?string $value): void
     {
         $node = new Node('cfdi', [$attribute => $value]);
         $cfdiVersion = new CfdiVersion();
