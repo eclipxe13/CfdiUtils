d82ca65071b1f5dfc5181b6ef8475a8acf625500
Carlos C Soto <eclipxe13@gmail.com>
2022-01-17 20:30:48 -0600

    Validate40TestCase should set Comprobante helper element

--- tests/CfdiUtilsTests/Validate/Validate40TestCase.php
+++ tests/CfdiUtilsTests/Validate/Validate40TestCase.php
@@ -2,16 +2,25 @@
 
 namespace CfdiUtilsTests\Validate;
 
-use CfdiUtils\Nodes\Node;
+use CfdiUtils\Elements\Cfdi40\Comprobante;
 
 abstract class Validate40TestCase extends ValidateBaseTestCase
 {
     protected function setUp(): void
     {
         parent::setUp();
-        $this->comprobante = new Node('cfdi:Comprobante', [
-            'xmlns:cfdi' => 'http://www.sat.gob.mx/cfd/4',
-            'Version' => '4.0',
-        ]);
+        $this->comprobante = new Comprobante();
+    }
+
+    /**
+     * Use this function to allow code analysis tools to perform correctly
+     * @return Comprobante
+     */
+    protected function getComprobante(): Comprobante
+    {
+        if ($this->comprobante instanceof Comprobante) {
+            return $this->comprobante;
+        }
+        throw new \RuntimeException('The current comprobante node is not a ' . Comprobante::class);
     }
 }
