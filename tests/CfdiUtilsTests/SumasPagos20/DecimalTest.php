e33aedb9e7adab8e45daf5a77f3874d1c4f4d13c
Carlos C Soto <eclipxe13@gmail.com>
2026-09-08 10:18:05 -0600

    Use bcround if PHP 8.4 or higher, also extend test

--- tests/CfdiUtilsTests/SumasPagos20/DecimalTest.php
+++ tests/CfdiUtilsTests/SumasPagos20/DecimalTest.php
@@ -11,10 +11,19 @@ final class DecimalTest extends TestCase
     public function providerRound(): array
     {
         return [
-            [new Decimal('45740.35'), new Decimal('45740.3490'), 2],
-            [new Decimal('1.23'), new Decimal('1.234'), 2],
-            [new Decimal('1.24'), new Decimal('1.235'), 2],
-            [new Decimal('1.24'), new Decimal('1.236'), 2],
+            '1.234 -> 1.23' => [new Decimal('1.23'), new Decimal('1.234'), 2],
+            '1.235 -> 1.24' => [new Decimal('1.24'), new Decimal('1.235'), 2],
+            '1.236 -> 1.24' => [new Decimal('1.24'), new Decimal('1.236'), 2],
+            '1.237 -> 1.24' => [new Decimal('1.24'), new Decimal('1.237'), 2],
+            '1.238 -> 1.24' => [new Decimal('1.24'), new Decimal('1.238'), 2],
+            '1.239 -> 1.24' => [new Decimal('1.24'), new Decimal('1.239'), 2],
+            '1.240 -> 1.24' => [new Decimal('1.24'), new Decimal('1.240'), 2],
+            '1.241 -> 1.24' => [new Decimal('1.24'), new Decimal('1.241'), 2],
+            '1.242 -> 1.24' => [new Decimal('1.24'), new Decimal('1.242'), 2],
+            '1.243 -> 1.24' => [new Decimal('1.24'), new Decimal('1.243'), 2],
+            '1.244 -> 1.24' => [new Decimal('1.24'), new Decimal('1.244'), 2],
+            '1.245 -> 1.25' => [new Decimal('1.25'), new Decimal('1.245'), 2],
+            '45740.3490 -> 45740.35' => [new Decimal('45740.35'), new Decimal('45740.3490'), 2],
         ];
     }
 
